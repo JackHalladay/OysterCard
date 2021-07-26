c2ad850e18070df6d88a104abfae6ef7451d3d22
Jack <jackhalladay@gmail.com>
2021-07-26 16:22:20 +0100

    cannot top up past a maximum

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,18 @@
 class OysterCard
 attr_reader :balance
-
+MAXIMUM_BALANCE = 90
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    @amount = amount
+    fail "Maximum balance of #{MAXIMUM_BALANCE} has been exceeded" if self.exceeded?
+    @balance += @amount
+  end
+
+  private
+  def exceeded?
+    (@balance + @amount) > MAXIMUM_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,19 @@ describe OysterCard do
   it "has a balance" do
     expect(subject.balance).to eq(0)
   end
+  
+  it 'can top up the balance' do
+    expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
+  end
+
+  it "won't exceed maximum balance" do
+    maximum_balance = OysterCard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{subject.top_up(1)}.to raise_error("Maximum balance of #{maximum_balance} has been exceeded")
+  end
+
+  it "can have money deducted" do
+    subject.top_up(30)
+    expect{subject.deduct(10)}.to change{subject.balance}.by -10
+  end
 end
