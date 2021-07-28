5254b04fca584f85adf2aeceda392903e712a377
Jack <jackhalladay@gmail.com>
2021-07-28 14:52:36 +0100

    Entry and exit station logs

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,14 @@
 class OysterCard
 attr_reader :balance
+attr_reader :entry_station
+attr_reader :exit_station
+attr_reader :journey_history
 MAXIMUM_BALANCE = 90
 MINIMUM_BALANCE = 1
   def initialize
+    @entry_station = nil
     @balance = 0
+    @journey_history = {}
   end
 
   def top_up(up_amount)
@@ -12,21 +17,20 @@ MINIMUM_BALANCE = 1
     @balance += @up_amount
   end
 
-  def deduct(down_amount)
-    @balance -= down_amount
-  end
-
-  def touch_in
+  def touch_in(entry_station)
+    @entry_station = entry_station
     fail "You do not have the minimum amount of #{MINIMUM_BALANCE}" if self.minimum?
-    @journey = true
   end
   
-  def touch_out
-    @journey = false
+  def touch_out(exit_station)
+    @exit_station = exit_station
+    deduct(MINIMUM_BALANCE)
+    add_journey
+    @entry_station = nil
   end
 
   def in_journey?
-    @journey
+    !!entry_station
   end
 
   private
@@ -37,4 +41,13 @@ MINIMUM_BALANCE = 1
   def minimum?
     @balance < MINIMUM_BALANCE
   end
+
+  def deduct(down_amount)
+    @balance -= down_amount
+  end
+
+  def add_journey
+    @journey_history = {entry: @entry_station, exit: @exit_station}
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,10 @@ require "OysterCard"
 
 describe OysterCard do
 
+  let(:entry_station){double :entry_station}
+  let(:exit_station){double :exit_station}
+  let(:journey){ {entry: entry_station, exit: exit_station} }
+
   it "has a balance" do
     expect(subject.balance).to eq(0)
   end
@@ -16,24 +20,38 @@ describe OysterCard do
     expect{subject.top_up(1)}.to raise_error("Maximum balance of #{maximum_balance} has been exceeded")
   end
 
-  it "can have money deducted" do
-    subject.top_up(30)
-    expect{subject.deduct(10)}.to change{subject.balance}.by -10
-  end
+  # it "can have money deducted" do
+  #   subject.top_up(30)
+  #   expect{subject.deduct(10)}.to change{subject.balance}.by -10
+  # end
 
   it "can log touching in" do
     subject.top_up(5)
-    subject.touch_in
+    subject.touch_in(entry_station)
     expect(subject.in_journey?).to eq(true)
   end
 
   it "can log touching out" do
-    subject.touch_out
+    subject.touch_out(exit_station)
     expect(subject.in_journey?).to eq(false)
   end
 
   it "needs a minimum of £1" do
     minimum_amount = OysterCard::MINIMUM_BALANCE
-    expect{subject.touch_in}.to raise_error("You do not have the minimum amount of #{minimum_amount}")
+    expect{subject.touch_in(entry_station)}.to raise_error("You do not have the minimum amount of #{minimum_amount}")
   end
+
+  it "dedcuts money on touch out" do
+    subject.top_up(5)
+    subject.touch_in(entry_station)
+    expect{subject.touch_out(exit_station)}.to change{subject.balance}.by(-OysterCard::MINIMUM_BALANCE)   
+  end
+
+  it "stores a journey" do
+    subject.top_up(5)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.journey_history).to include journey
+  end
+
 end
